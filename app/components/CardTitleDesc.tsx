f3e701ec85eb97eb50eeb44e44a33d1c9725e4f1
sampath-ops <sampathkaali002@gmail.com>
2024-07-13 19:20:37 +0530

    experience year order changed

--- app/components/CardTitleDesc.tsx
+++ app/components/CardTitleDesc.tsx
@@ -6,7 +6,7 @@ export const CardTitleDesc = ({ cardContent }:any) => {
         <div className="grid sm:grid-cols-8 gap-4 lg:gap-8 relative group transition-all lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
           <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
           {cardContent?.duration && (
-            <header className="relative sm:col-span-2 sm:order-1 z-10 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 order-2">
+            <header className="relative sm:col-span-2 sm:order-1 z-10 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500">
               {cardContent.duration}
             </header>
           )}
